2bb0244358a46590282f7720892a5a9cd26c3ea4
犬走椛 <moemoechu@yeah.net>
2023-11-18 19:01:31 +0800

    fix.

--- src/translations.ts
+++ src/translations.ts
@@ -8,7 +8,7 @@ export const translations: [string, HighlightPluginTranslations][] = [
       "Enable highlight": "启用高亮",
       "Highlight master switch": "高亮总开关",
       "Change any setting of highlight only apply in new sessions.":
-        "任何对高亮设置的修改只应用到新会话。",
+        "任何对高亮设置的修改只会应用到新会话。",
       "Highlight applied!": "高亮已应用！",
       "ON": "开",
       "Keyword": "关键字",
